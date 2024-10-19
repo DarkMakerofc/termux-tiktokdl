b1488af9267519277576fc9efb350dce22d57fc5
Nimesh Madusanka <104353654+MR-NIMA-X@users.noreply.github.com>
2024-10-19 12:07:09 +0530

    add HD and mp3 downloader

--- src/code.js
+++ src/code.js
@@ -3,9 +3,13 @@ const { downloadTiktok } = require("@mrnima/tiktok-downloader");
 module.exports.download = async (link) => {
     try {
         const { result } = await downloadTiktok(link);
+        //console.log(result);
         return {
-            link: result.dl_link.download_mp4_1,
-            title: result.title.replace(/ /g, "-")
+            images: result.dl_link.images || false,
+            link: result.dl_link.download_mp4_1 || false,
+            mp3: result.dl_link.download_mp3 || false,
+            HD: result.dl_link.download_mp4_hd || false, 
+            title: result.title.replace(/ /g, "-").replace(/”/g,"").replace(/"/g,"").slice(0,90)
         };
     } catch (error) {
         console.error("Error downloading TikTok video:", error);
